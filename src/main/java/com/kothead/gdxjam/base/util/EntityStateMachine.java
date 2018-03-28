9fd512cfa27f7a277657d9159f92dcf468323474
kothead <stopkillingshoggoths@gmail.com>
2018-03-28 23:04:33 +0500

    Add state machine for entities

---
+++ src/main/java/com/kothead/gdxjam/base/util/EntityStateMachine.java
@@ -0,0 +1,16 @@
+package com.kothead.gdxjam.base.util;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.fsm.DefaultStateMachine;
+import com.badlogic.gdx.ai.fsm.State;
+
+public class EntityStateMachine extends DefaultStateMachine<Entity, State<Entity>> {
+
+    public EntityStateMachine(Entity owner, State<Entity> initialState) {
+        super(owner, initialState);
+    }
+
+    public EntityStateMachine(Entity owner, State<Entity> initialState, State<Entity> globalState) {
+        super(owner, initialState, globalState);
+    }
+}
