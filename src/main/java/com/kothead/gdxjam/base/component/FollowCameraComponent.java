8bdca6949284ffa1eb9f4d293d4685cd9e95a609
kothead <stopkillingshoggoths@gmail.com>
2018-12-02 00:21:04 +0500

    Add mapper to follow camera component

--- src/main/java/com/kothead/gdxjam/base/component/FollowCameraComponent.java
+++ src/main/java/com/kothead/gdxjam/base/component/FollowCameraComponent.java
@@ -1,6 +1,9 @@
 package com.kothead.gdxjam.base.component;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.ComponentMapper;
 
 public class FollowCameraComponent implements Component {
+
+    public static final ComponentMapper<FollowCameraComponent> mapper = ComponentMapper.getFor(FollowCameraComponent.class);
 }
